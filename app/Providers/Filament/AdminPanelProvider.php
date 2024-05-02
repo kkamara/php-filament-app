235a4c2390e595bbf1e3e10a892f079f721967c8
kkamara <kamaracomputers@gmail.com>
2024-05-02 10:51:21 +0100

    Add brand logo and favicon

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -36,6 +36,8 @@ class AdminPanelProvider extends PanelProvider
                 'warning' => Color::Orange,
             ])
             ->font('Inter')
+            ->favicon(asset('images/logo.jpg'))
+            ->brandLogo(asset('images/logo.jpg'))
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
