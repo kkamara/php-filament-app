7e6bdb85e6935dd1a69216397549ff5a27e8ac9f
kkamara <kamaracomputers@gmail.com>
2024-05-03 15:00:11 +0100

    Add table relationship columns for Employees screen

--- app/Filament/Resources/EmployeeResource.php
+++ app/Filament/Resources/EmployeeResource.php
@@ -117,18 +117,18 @@ class EmployeeResource extends Resource
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('country_id')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('state_id')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('city_id')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('department_id')
-                    ->numeric()
-                    ->sortable(),
+                Tables\Columns\TextColumn::make('country.name')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('state.name')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('city.name')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('department.name')
+                    ->sortable()
+                    ->searchable(),
                 Tables\Columns\TextColumn::make('first_name')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('last_name')
