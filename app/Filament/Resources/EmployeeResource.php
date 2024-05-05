c4348e519b1001ce53d7921a8e8db7bfc6716021
kkamara <kamaracomputers@gmail.com>
2024-05-05 18:57:35 +0100

    Update notification title for deleting Employee

--- app/Filament/Resources/EmployeeResource.php
+++ app/Filament/Resources/EmployeeResource.php
@@ -256,7 +256,10 @@ class EmployeeResource extends Resource
             ->actions([
                 Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
+                Tables\Actions\DeleteAction::make()
+                    ->successNotificationTitle(
+                        "Employee deleted."
+                    ),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
