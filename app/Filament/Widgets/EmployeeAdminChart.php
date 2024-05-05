d9ca9389f8caf3fc8faa3a5409805ab9e2a04a53
kkamara <kamaracomputers@gmail.com>
2024-05-05 23:45:39 +0100

    Add hardcoded chart data and colour scheme to Admin dashboard

--- app/Filament/Widgets/EmployeeAdminChart.php
+++ app/Filament/Widgets/EmployeeAdminChart.php
@@ -6,17 +6,25 @@ use Filament\Widgets\ChartWidget;
 
 class EmployeeAdminChart extends ChartWidget
 {
-    protected static ?string $heading = 'Chart';
+    protected static ?string $heading = 'Employees Chart';
+
+    protected static string $color = 'warning';
 
     protected function getData(): array
     {
         return [
-            //
+            'datasets' => [
+                [
+                    'label' => 'Blog posts created',
+                    'data' => [0, 10, 5, 2, 21, 32, 45, 74, 65, 45, 77, 89],
+                ],
+            ],
+            'labels' => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
         ];
     }
 
     protected function getType(): string
     {
-        return 'bar';
+        return 'line';
     }
 }
