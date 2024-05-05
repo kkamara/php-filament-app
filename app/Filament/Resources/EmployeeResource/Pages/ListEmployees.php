ba7f7c2a9e42d3c60c688d827c0c2dc242244016
kkamara <kamaracomputers@gmail.com>
2024-05-05 16:43:56 +0100

    Add custom table tabs to Employees Screen

--- app/Filament/Resources/EmployeeResource/Pages/ListEmployees.php
+++ app/Filament/Resources/EmployeeResource/Pages/ListEmployees.php
@@ -5,6 +5,9 @@ namespace App\Filament\Resources\EmployeeResource\Pages;
 use App\Filament\Resources\EmployeeResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Pages\ListRecords\Tab;
+use Illuminate\Database\Eloquent\Builder;
+use App\Models\Employee;
 
 class ListEmployees extends ListRecords
 {
@@ -14,6 +17,51 @@ class ListEmployees extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array {
+        return [
+            "All" => Tab::make(),
+            "This Week" => Tab::make()
+                ->modifyQueryUsing(
+                    fn (Builder $query) => $query
+                        ->where("date_hired", ">=", now()->subWeek())
+                )
+                ->badge(
+                    Employee::query()
+                        ->where(
+                            "date_hired",
+                            ">=",
+                            now()->subWeek()
+                        )->count()
+                ),
+            "This Month" => Tab::make()
+                ->modifyQueryUsing(
+                    fn (Builder $query) => $query
+                        ->where("date_hired", ">=", now()->subMonth())
+                )
+                ->badge(
+                    Employee::query()
+                        ->where(
+                            "date_hired",
+                            ">=",
+                            now()->subMonth()
+                        )->count()
+                ),
+            "This Year" => Tab::make()
+                ->modifyQueryUsing(
+                    fn (Builder $query) => $query
+                        ->where("date_hired", ">=", now()->subYear())
+                )
+                ->badge(
+                    Employee::query()
+                        ->where(
+                            "date_hired",
+                            ">=",
+                            now()->subYear()
+                        )->count()
+                ),
         ];
     }
 }
