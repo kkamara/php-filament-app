a3aeff63a2d5d059f97697c171d88b4340ea53ca
kkamara <kamaracomputers@gmail.com>
2024-05-05 23:55:33 +0100

    Add dynamic data for UserAdminChart

--- app/Filament/Widgets/UserAdminChart.php
+++ app/Filament/Widgets/UserAdminChart.php
@@ -2,16 +2,33 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\User;
 use Filament\Widgets\ChartWidget;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
 
 class UserAdminChart extends ChartWidget
 {
-    protected static ?string $heading = 'Chart';
+    protected static ?string $heading = 'Users Chart';
 
     protected function getData(): array
     {
+        $data = Trend::model(User::class)
+            ->between(
+                start: now()->startOfMonth(),
+                end: now()->endOfMonth(),
+            )
+            ->perDay()
+            ->count();
+    
         return [
-            //
+            'datasets' => [
+                [
+                    'label' => 'Users',
+                    'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
+                ],
+            ],
+            'labels' => $data->map(fn (TrendValue $value) => $value->date),
         ];
     }
 
