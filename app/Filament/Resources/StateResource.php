f9c449a784dfe4e1bc4b2e3227d8eda6bfb38a9f
kkamara <kamaracomputers@gmail.com>
2024-05-05 17:17:52 +0100

    Add Cities and Employees Relation Managers to States Screen

--- app/Filament/Resources/StateResource.php
+++ app/Filament/Resources/StateResource.php
@@ -4,6 +4,8 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\StateResource\Pages;
 use App\Filament\Resources\StateResource\RelationManagers;
+use App\Filament\Resources\StateResource\RelationManagers\CitiesRelationManager;
+use App\Filament\Resources\StateResource\RelationManagers\EmployeesRelationManager;
 use App\Models\State;
 use Filament\Forms;
 use Filament\Forms\Form;
@@ -15,7 +17,6 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Filament\Infolists\Components\TextEntry;
 use Filament\Infolists\Components\Section;
-
 
 class StateResource extends Resource
 {
@@ -101,7 +102,8 @@ class StateResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            CitiesRelationManager::Class,
+            EmployeesRelationManager::class,
         ];
     }
 
