7ab9e8b41b67a89dcdf8e2b2483170b61e32bfad
kkamara <kamaracomputers@gmail.com>
2024-05-05 18:55:55 +0100

    Add table item delete action to Employees Screen

--- app/Filament/Resources/EmployeeResource.php
+++ app/Filament/Resources/EmployeeResource.php
@@ -256,6 +256,7 @@ class EmployeeResource extends Resource
             ->actions([
                 Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
